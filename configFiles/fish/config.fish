ad97ac56e73c08c4be1a40c2eef98ac34c9cceff
haimeh <haimehs@gmail.com>
2021-08-25 18:06:06 -0600

    more git fish

--- configFiles/fish/config.fish
+++ configFiles/fish/config.fish
@@ -56,7 +56,7 @@ function fish_mode_prompt
     case replace_one
       set_color --bold green
       echo '[R]'
-    case visual
+    case visua
       set_color --bold brmagenta
       echo '[V]'
     case '*'
@@ -75,11 +75,14 @@ set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "+"
 set -g __fish_git_prompt_char_upstream_behind "-"
 set -g __fish_git_prompt_char_upstream_prefix " "
+set -g __fish_git_prompt_char_upstream_diverged '<>'
+set -g __fish_git_prompt_char_upstream_equal '=='
+
 set -g __fish_git_prompt_char_stateseparator " "
 
 set -g __fish_git_prompt_char_stagedstate "#"
 set -g __fish_git_prompt_char_dirtystate "!"
-set -g __fish_git_prompt_char_untrackedfiles "-"
+set -g __fish_git_prompt_char_untrackedfiles " ..."
 set -g __fish_git_prompt_char_conflictedstate "x"
 set -g __fish_git_prompt_char_cleanstate "="
 
@@ -88,6 +91,7 @@ set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 set -g __fish_git_prompt_color_cleanstate green
+set -g __fish_git_prompt_char_stashstate '$'
 
 function fish_prompt
     # for some reason (fish_git_prompt "\0") doesnt work when called from echo
